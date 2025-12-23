7db4300791828799a8761ad425ef1675ce6db39b
Mathias <peyre.mathias@gmail.com>
2025-12-23 10:22:58 +0100

    Rename project to europi-contrib-docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
+  site: 'https://s0ca.github.io',
+  base: '/europi-contrib-docs',
   integrations: [
     starlight({
-      title: 'EuroPi Contrib Documentation',
-      description: 'Friendly docs generated from Markdown.',
+      title: 'EuroPi Contrib Docs',
+      description: 'Documentation for community EuroPi scripts.',
       defaultLocale: 'en',
       components: {
         Footer: './src/components/StickyFooter.astro',
